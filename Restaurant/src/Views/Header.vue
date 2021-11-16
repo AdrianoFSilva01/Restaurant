3839adc4075562bbc00c616bb96783cfd9c5d817
Adriano Silva <AdrianoSilva16601@outlook.com>
2021-11-16 11:32:15 +0000

    Add Header

--- Restaurant/src/Views/Header.vue
+++ Restaurant/src/Views/Header.vue
@@ -1,5 +1,21 @@
 <template>
-    <div class="w-full bg-red-500 h-10">
-        Ola
+    <div class="flex p-8 items-center">
+        <div class="leading-3 text-sm">
+            Order
+            <br />
+            Now
+        </div>
+        <div class="flex flex-grow justify-center h-14">
+            <img src="@/Assets/Images/logo.svg" />
+        </div>
+        <div class="hamburgerMenu" @click="hamburgerMenuOnClick">
+            <hr />
+            <hr />
+            <hr />
+        </div>
     </div>
 </template>
+
+<script lang="ts" src="./Header.ts" />
+
+<style lang="postcss" src="./Header.pcss" />
