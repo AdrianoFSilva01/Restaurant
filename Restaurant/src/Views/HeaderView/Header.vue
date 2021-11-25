283e32b7a32f576b4d038ba23fac979c614c45b8
Adriano Silva <AdrianoSilva16601@outlook.com>
2021-11-25 15:20:55 +0000

    Fix items centered

--- Restaurant/src/Views/HeaderView/Header.vue
+++ Restaurant/src/Views/HeaderView/Header.vue
@@ -1,17 +1,19 @@
 <template>
-    <div class="flex p-8 justify-between items-center">
-        <div class="leading-3 text-sm">
+    <div class="flex p-8 items-center">
+        <div class="leading-3 text-sm flex-1">
             Order
             <br />
             Now
         </div>
-        <div class="flex justify-center h-14">
+        <div class="flex justify-center h-14 flex-1">
             <img src="@/Assets/Images/logo.svg" />
         </div>
-        <div class="hamburgerMenu" @click="hamburgerMenuOnClick">
-            <hr />
-            <hr />
-            <hr />
+        <div class="flex-1 flex justify-end">
+            <div class="hamburgerMenu" @click="hamburgerMenuOnClick">
+                <hr />
+                <hr />
+                <hr />
+            </div>
         </div>
     </div>
 </template>
