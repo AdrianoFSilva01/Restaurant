ab413b7eb64876d60577ffb3760042c7e1da66b7
Adriano Silva <AdrianoSilva16601@outlook.com>
2021-12-23 12:44:59 +0000

    Fix Overlay scroll

--- Restaurant/src/MasterPage/MasterPage.ts
+++ Restaurant/src/MasterPage/MasterPage.ts
@@ -3,6 +3,7 @@ import Header from "@/Views/HeaderView/Header.vue";
 import MainView from "@/Views/MainView/MainView.vue";
 import Overlay from "@/Views/OverlayView/Overlay.vue";
 import { Options, Vue } from "vue-class-component";
+import { Watch } from "vue-property-decorator";
 
 @Options({
     components: {
@@ -15,4 +16,13 @@ import { Options, Vue } from "vue-class-component";
 
 export default class MasterPage extends Vue {
     displayOverlay: boolean = false;
+
+    @Watch(nameof((masterPage: MasterPage) => masterPage.displayOverlay))
+    onCurrentIndexChange(): void {
+        if(this.displayOverlay) {
+            document.getElementsByTagName("html")[0].style.overflow = "hidden";
+        } else {
+            document.getElementsByTagName("html")[0].style.overflow = "visible";
+        }
+    }
 }
