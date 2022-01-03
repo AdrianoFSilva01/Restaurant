6ccf67629ab80ffb7db6ee3a4c9683b71cea5c3b
Adriano Silva <AdrianoSilva16601@outlook.com>
2022-01-03 11:47:11 +0000

    Responsive design

--- Restaurant/src/Views/HeaderView/Header.vue
+++ Restaurant/src/Views/HeaderView/Header.vue
@@ -1,23 +1,31 @@
 <template>
     <div class="headerContainer">
-        <div class="navigationText">
+        <nav class="navigationText">
             Order
             <br />
             Now
-        </div>
+        </nav>
         <div ref="imageElement" class="navigationImage">
             <img src="@/Assets/Images/logo.svg" />
         </div>
-        <div class="navigationIcon">
+        <nav class="navigationIcon">
             <div class="hamburgerMenu" @click="hamburgerMenuOnClick">
                 <hr />
                 <hr />
                 <hr />
             </div>
+            <a class="navigationPages">
+                Order
+            </a>
+        </nav>
+        <div class="navigationButton">
+            <button class="button">
+                ORDER NOW
+            </button>
         </div>
     </div>
 </template>
 
 <script lang="ts" src="./Header.ts" />
 
-<style scoped lang="postcss" src="./Header.pcss" />
+<style scoped lang="postcss" src="./Styles/Header.pcss" />
