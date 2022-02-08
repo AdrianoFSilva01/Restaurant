a0c15d2fe4df3d278b1f6911be93a37b32bbb741
Adriano Silva <AdrianoSilva16601@outlook.com>
2022-02-08 13:08:59 +0000

    Change class names

--- Restaurant/src/Components/InlineList/InlineList.vue
+++ Restaurant/src/Components/InlineList/InlineList.vue
@@ -1,11 +1,11 @@
 <template>
-    <div class="inlineListcontainer">
+    <div class="inline-list-container">
         <template v-for="(name, index) in list" :key="index">
-            <div class="listItem">
+            <div class="list-item">
                 <template v-if="index > 0">
                     <hr class="w-4 mx-2 border-black" />
                 </template>
-                <p class="listText" :class="{ 'opacity-30' : index !== currentIndex }" @click="onClick">
+                <p class="list-text" :class="{ 'opacity-30' : index !== currentIndex }" @click="onClick">
                     {{ name }}
                 </p>
             </div>
@@ -15,4 +15,4 @@
 
 <script lang="ts" src="./InlineList.ts" />
 
-<style scoped lang="postcss" src="./InlineList.pcss" />
+<style scoped lang="scss" src="./InlineList.scss" />
