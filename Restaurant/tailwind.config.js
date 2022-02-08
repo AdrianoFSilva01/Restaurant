e8c0c4deee9aeb500b08b899902c233a90ad5003
Adriano Silva <AdrianoSilva16601@outlook.com>
2022-02-08 13:15:53 +0000

    Update tailwind.config

--- Restaurant/tailwind.config.js
+++ Restaurant/tailwind.config.js
@@ -8,19 +8,60 @@ module.exports = {
         extend: {
             colors: {
                 ...colors,
-                gold: "#d6a159"
+                gold: "#d6a159",
+                darkGold: "#785a31"
             },
             scale: {
                 '-100' : '-1'
             },
             height: {
                 '70vh' : '70vh'
+            },
+            transitionDuration: {
+                400: "400ms",
+            },
+            cursor: {
+                grab: "grab",
+                grabbing: "grabbing"
+            },
+            animation: {
+                "wave-button-animation": " wave-button-animation .4s forwards",
+                "wave-button-animation-reverse": "wave-button-animation-reverse .4s",
+            },
+            keyframes: {
+                "wave-button-animation": {
+                    "0%": {
+                        transform: "translateY(0%)"
+                    },
+                    "50%": {
+                        transform: "translateY(-100%)"
+                    },
+                    "50.1%": {
+                        transform: "translateY(100%)"
+                    },
+                    "100%": {
+                        transform: "translateY(0%)"
+                    }
+                },
+                "wave-button-animation-reverse": {
+                    "0%": {
+                        transform: "translateY(0%)"
+                    },
+                    "50%": {
+                        transform: "translateY(100%)"
+                    },
+                    "50.1%": {
+                        transform: "translateY(-100%)"
+                    },
+                    "100%": {
+                        transform: "translateY(0%)"
+                    }
+                }
             }
         },
         fontFamily: {
             sans: ["Segoe UI"]
-        },
-
+        }
     },
     purge: ["./public/**/*.html", "./src/**/*.{vue,ts}"],
     variants: {
