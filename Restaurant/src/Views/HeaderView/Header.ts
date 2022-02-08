2017940b6c5ee6199949d4a6964ef420db6b5eff
Adriano Silva <AdrianoSilva16601@outlook.com>
2022-02-08 13:14:06 +0000

    Change class names and pcss files to scss

--- Restaurant/src/Views/HeaderView/Header.ts
+++ Restaurant/src/Views/HeaderView/Header.ts
@@ -1,15 +1,19 @@
+import WaveButton from "@/Components/WaveButton/WaveButton.vue";
 import { Options, Vue } from "vue-class-component";
 import { Ref } from "vue-property-decorator";
 
 @Options({
+    components: {
+        WaveButton
+    },
     emits: [
         "hamburger-menu-clicked"
     ]
 })
 export default class Header extends Vue {
+    @Ref() headerContainer!: HTMLElement;
     @Ref() imageElement!: HTMLElement;
 
-    element!: HTMLElement;
     elementPadding: number = 0;
     elementHeight: number = 0;
     scrollPosition: number = 0;
@@ -17,34 +21,40 @@ export default class Header extends Vue {
     imagePaddingWhenSticky: number = 12;
 
     mounted(): void {
-        this.element = this.$el as HTMLElement;
-        this.elementHeight = this.element.getHeight();
-        this.elementPadding = this.element.getPaddingTop();
-        this.elementTransition = this.element.getTransition();
+        this.elementHeight = this.headerContainer.getHeight();
+        this.elementPadding = this.headerContainer.getPaddingTop();
+        this.elementTransition = this.headerContainer.getTransition();
+
+        (this.$el as HTMLElement).style.height = `${this.elementHeight}px`;
 
         window.addEventListener("scroll", () => {
-            this.element.style.transition = this.elementTransition;
+            this.headerContainer.style.transition = this.elementTransition;
 
             if(this.scrollPosition < window.scrollY) {
-                this.element.style.top = "-100%";
+                if(window.scrollY < this.elementHeight) {
+                    this.headerContainer.style.transition = "none";
+                    this.headerContainer.style.transform = `translateY(-${window.scrollY}px)`;
+                } else {
+                    this.headerContainer.style.transform = "translateY(-100%)";
+                }
             } else if(this.scrollPosition > window.scrollY) {
                 if(window.scrollY > this.elementHeight * 2) {
                     this.imageElement.style.padding = `${this.imagePaddingWhenSticky}px`;
-                    this.element.style.paddingTop = "0";
-                    this.element.style.paddingBottom = "0";
-                    this.element.style.top = "0px";
-                    this.element.classList.add("shadow-md");
+                    this.headerContainer.style.paddingTop = "0";
+                    this.headerContainer.style.paddingBottom = "0";
+                    this.headerContainer.style.transform = "translateY(0)";
+                    this.headerContainer.classList.add("shadow-md");
                 } else if(window.scrollY <= this.elementHeight && window.scrollY >= 0) {
                     const percentage: number = (window.scrollY / this.elementHeight);
                     const imageElementPadding: number = percentage * this.imagePaddingWhenSticky;
                     const padding: number = (1 - percentage) * this.elementPadding;
 
-                    this.element.style.transition = "none";
-
-                    this.element.style.paddingTop = `${padding}px`;
-                    this.element.style.paddingBottom = `${padding}px`;
+                    this.headerContainer.style.transition = "none";
+                    this.headerContainer.style.transform = "translateY(0)";
+                    this.headerContainer.style.paddingTop = `${padding}px`;
+                    this.headerContainer.style.paddingBottom = `${padding}px`;
                     this.imageElement.style.padding = `${imageElementPadding}px`;
-                    this.element.classList.remove("shadow-md");
+                    this.headerContainer.classList.remove("shadow-md");
                 }
             }
 
@@ -54,5 +64,9 @@ export default class Header extends Vue {
 
     hamburgerMenuOnClick(): void {
         this.$emit("hamburger-menu-clicked");
+    }
+
+    scrollToTop(): void {
+        document.documentElement.scrollTop = 0;
     }
 }
--- Restaurant/src/Views/HeaderView/Header.vue
+++ Restaurant/src/Views/HeaderView/Header.vue
@@ -1,31 +1,41 @@
 <template>
-    <div class="headerContainer">
-        <nav class="navigationText">
-            Order
-            <br />
-            Now
-        </nav>
-        <div ref="imageElement" class="navigationImage">
-            <img src="@/Assets/Images/logo.svg" />
-        </div>
-        <nav class="navigationIcon">
-            <div class="hamburgerMenu" @click="hamburgerMenuOnClick">
-                <hr />
-                <hr />
-                <hr />
+    <div class="sticky top-0 z-40">
+        <div ref="headerContainer" class="header-container" v-delayed-childs="[0.1, 'delayedAnimation', 'transform-opacity-animation', true]">
+            <nav class="navigation-text delayedAnimation">
+                <router-link to="/order" @click="scrollToTop">
+                    Order
+                    <br />
+                    Now
+                </router-link>
+            </nav>
+            <router-link to="/" class="delayedAnimation" @click="scrollToTop">
+                <div ref="imageElement" class="navigation-image">
+                    <img src="@/Assets/Images/RestaurantIcon.svg" />
+                </div>
+            </router-link>
+            <nav class="navigation-icon delayedAnimation">
+                <div class="hamburger-menu" @click="hamburgerMenuOnClick">
+                    <hr />
+                    <hr />
+                    <hr />
+                </div>
+                <a class="navigation-pages">
+                    <router-link to="/order" @click="scrollToTop">
+                        Order
+                    </router-link>
+                </a>
+            </nav>
+            <div class="navigation-button delayedAnimation">
+                <div class="button" @click="scrollToTop">
+                    <router-link to="/order">
+                        <WaveButton buttontext="ORDER NOW" />
+                    </router-link>
+                </div>
             </div>
-            <a class="navigationPages">
-                Order
-            </a>
-        </nav>
-        <div class="navigationButton">
-            <button class="button">
-                ORDER NOW
-            </button>
         </div>
     </div>
 </template>
 
 <script lang="ts" src="./Header.ts" />
 
-<style scoped lang="postcss" src="./Styles/Header.pcss" />
+<style scoped lang="scss" src="./Styles/Header.scss" />
