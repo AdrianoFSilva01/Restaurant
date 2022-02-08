9f570234f84707c894afb86cc73b89f8a0d5959d
Adriano Silva <AdrianoSilva16601@outlook.com>
2022-02-08 13:10:50 +0000

    Add directives

--- Restaurant/src/Extensions/Directives/installDirectives.ts
+++ Restaurant/src/Extensions/Directives/installDirectives.ts
@@ -1,6 +1,12 @@
 import delayedChilds from "@/Extensions/Directives/CssDirectives/dealyed-childs";
+import clickOutside from "@/Extensions/Directives/JsDirectives/click-outside";
 import { App } from "vue";
+import firstHover from "./CssDirectives/first-hover";
+import inViewport from "./CssDirectives/in-viewport";
 
 export default function installDirectives(app: App<Element>): void {
     app.directive("delayed-childs", delayedChilds);
+    app.directive("in-viewport", inViewport);
+    app.directive("click-outside", clickOutside);
+    app.directive("first-hover", firstHover);
 }
