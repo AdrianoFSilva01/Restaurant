e78caea8c54f9d5395cfec2d94b1f5008a839ab9
Adriano Silva <AdrianoSilva16601@outlook.com>
2022-02-08 13:10:22 +0000

    Fix delayed-childs bug for mouted elements

--- Restaurant/src/Extensions/Directives/CssDirectives/dealyed-childs.ts
+++ Restaurant/src/Extensions/Directives/CssDirectives/dealyed-childs.ts
@@ -1,7 +1,26 @@
 import { DirectiveBinding } from "@vue/runtime-core";
+import { nextTick } from "vue";
 
 export default {
-    updated: function (el: HTMLElement, binding: DirectiveBinding): void {
+    mounted: function(el: HTMLElement, binding: DirectiveBinding): void {
+        const delayValue: number = binding.value[0];
+        const className: string = binding.value[1];
+        const classToAdd: string = binding.value[2];
+        const execOnMount: boolean = binding.value[3];
+
+        if(execOnMount) {
+            nextTick(() => {
+                const childs: HTMLCollection = el.getElementsByClassName(className);
+
+                for(let i: number = 0; i < childs.length; i++) {
+                    setTimeout(() => {
+                        (childs[i] as HTMLElement).classList.add(classToAdd);
+                    }, (delayValue * i) * 1000);
+                }
+            });
+        }
+    },
+    updated: function(el: HTMLElement, binding: DirectiveBinding): void {
         const delayValue: number = binding.value[0];
         const className: string = binding.value[1];
         const classToAdd: string = binding.value[2];
@@ -9,6 +28,10 @@ export default {
         const childs: HTMLCollection = el.getElementsByClassName(className);
 
         for(let i: number = 0; i < childs.length; i++) {
+            if(childs[i].classList.contains(classToAdd)) {
+                return;
+            }
+
             setTimeout(() => {
                 (childs[i] as HTMLElement).classList.add(classToAdd);
             }, (delayValue * i) * 1000);
