13b27a8764a13dfe53acc9bcc7fabfcfb7a7b1e8
Adriano Silva <AdrianoSilva16601@outlook.com>
2022-02-08 13:12:24 +0000

    Add router and axios

--- Restaurant/src/MasterPage/MasterPage.ts
+++ Restaurant/src/MasterPage/MasterPage.ts
@@ -1,9 +1,13 @@
+import { CategoryState, useStore } from "@/category.store";
+import Category from "@/Models/Category";
 import Footer from "@/Views/FooterView/FooterView.vue";
 import Header from "@/Views/HeaderView/Header.vue";
 import MainView from "@/Views/MainView/MainView.vue";
 import Overlay from "@/Views/OverlayView/Overlay.vue";
+import { Axios, AxiosResponse } from "axios";
 import { Options, Vue } from "vue-class-component";
-import { Watch } from "vue-property-decorator";
+import { Inject, Watch } from "vue-property-decorator";
+import { Store } from "vuex";
 
 @Options({
     components: {
@@ -15,7 +19,18 @@ import { Watch } from "vue-property-decorator";
 })
 
 export default class MasterPage extends Vue {
+    @Inject() axios!: Axios;
+
     displayOverlay: boolean = false;
+
+    created(): void {
+        const store: Store<CategoryState> = useStore();
+
+        this.axios.get<Array<Category>>("Category/all")
+            .then((response: AxiosResponse<Array<Category>>) => {
+                store.commit("loadCategories", response.data);
+            });
+    }
 
     @Watch(nameof((masterPage: MasterPage) => masterPage.displayOverlay))
     onCurrentIndexChange(): void {
--- Restaurant/src/MasterPage/MasterPage.vue
+++ Restaurant/src/MasterPage/MasterPage.vue
@@ -1,14 +1,14 @@
 <template>
-    <div id="app">
+    <div id="app" class="flex flex-col" style="min-height: 100vh">
         <transition name="fade">
             <Overlay v-if="displayOverlay" :display="displayOverlay" @close-button-clicked="displayOverlay = false"  />
         </transition>
         <Header @hamburger-menu-clicked="displayOverlay = true" />
-        <MainView class="mainView" />
+        <router-view class="mainView" />
         <Footer />
     </div>
 </template>
 
 <script lang="ts" src="./MasterPage.ts" />
 
-<style scoped lang="postcss" src="./MasterPage.pcss" />
+<style scoped lang="scss" src="./MasterPage.scss" />
